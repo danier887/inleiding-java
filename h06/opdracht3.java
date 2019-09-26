345d54c815e9b233dc6817e68874404f4e3417b5
danier887 <54796566+danier887@users.noreply.github.com>
2019-09-26 09:17:47 +0200

    Add files via upload

--- h06/opdracht3.java
+++ h06/opdracht3.java
@@ -1,19 +1,36 @@
 package h06;
 
 import java.applet.Applet;
+import java.awt.*;
 
 public class opdracht3 extends Applet {
 
 
+    // globaal
+    int positiefgetal;
+    int Positiefgetal1;
+    int negatiefgetal;
+    int uitkomst;
+
+
+    public void init() {
+
+positiefgetal= 10;
+Positiefgetal1=10;
+negatiefgetal=5;
+
+uitkomst = (positiefgetal+Positiefgetal1) /negatiefgetal;
+
+
+    }
 
 
 
+    public void paint(Graphics g){
 
+        g.drawString("de uitkomst is:" + uitkomst, 20, 20);
 
-
-
-
-
+    }
 
 
 
